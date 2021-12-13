abe8944e229ebc5873556ac639d3ba595e3c3225
MajkeltCZe <majkeltcz@gmail.com>
2021-12-13 19:39:08 +0100

    walking demo 0.2

--- js/script.js
+++ js/script.js
@@ -2,99 +2,109 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 var character = new Image();
 document.addEventListener('keydown', keyboard);
+ document.addEventListener('keyup', keyboardPress);
+
 const frameWidth = 108;
 const frameHeight = 140;
 character.src = './img/test.png';
 
 var xPos = 20;
 var yPos = 550;
-var srcX;
-var srcY;
-var speed = 10;
-const fps = 60;
-const secondsToUpdate = 0.06 * fps;
+var speed = 5;
 let frameIndex = 0;
 let count = 0;
+var timer;
 
-// stavy animaci
-const State = {
-    states: {},
-    generateState: function (name, startIndex, endIndex, row) {
-        if (!this.states[name]) {
-            this.states[name] = {
-                frameIndex: startIndex,
-                startIndex: startIndex,
-                endIndex: endIndex,
-                row: row
-            };
-        }
-    },
-    getState: function (name) {
-        if (this.states[name]) {
-            return this.states[name];
-        }
+function animate(startIndex,endIndex,row) {
+ 
+ 
+    count++;
+    if (count > 5) {
+
+        frameIndex++;
+        count = 0;
+    
     }
-};
 
-State.generateState("right", 0, 6, 2);
-State.generateState("left", 0, 6, 3);
-
-
-function animate(state) {
+    if (frameIndex > endIndex) {
+        frameIndex = startIndex;
+    }
+ 
+ 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.drawImage(
-        character, state.frameIndex * frameWidth, srcY, frameWidth, frameHeight, xPos, yPos, frameWidth, frameHeight);
+    ctx.drawImage( character, frameIndex * frameWidth, row * frameHeight, frameWidth, frameHeight, xPos, yPos, frameWidth, frameHeight);
+
+    
 
 
-    srcY = state.row * frameHeight;
-
-    count++;
-    if (count > secondsToUpdate) {
-        state.frameIndex++;
-        count = 0;
-    }
-
-    if (state.frameIndex > state.endIndex) {
-        state.frameIndex = state.startIndex;
-    }
+ 
 }
 
 
 
+function interval() {
 
-function keyboard(e) {
-    if (e.keyCode == 68) {
-a = true;
+    var timesRun = 0;
+    timesRun += 1;
+    if (timesRun === 1) {
 
+       animate(0,0,4);
 
-        requestAnimationFrame(function() {
-
-            animate(State.getState("right"));
-         });
-
-
-
-        xPos += speed;
-
+        clearInterval(timer);
+       
     }
 
-    if (e.keyCode == 65) {
-        a = true;
-
-        requestAnimationFrame(function() {
-
-            animate(State.getState("left"));
-         });
-
-        xPos -= speed;
-
-
-    }
-
+  //  animate(State.getState("stand"));
 
 
 }
 
 
+function keyboardPress(b) {
+
+    if (b.keyCode == 68 || b.keyCode == 65) {
+
+
+        timer = setInterval(interval, 1);
+
+    }
+
+}
+
+
+
+
+    function keyboard(e) {
+        if (e.keyCode == 68) {
+            
+
+            requestAnimationFrame(function () {
+                clearInterval(timer);
+                animate(0,6,2);
+            });
+
+
+
+            xPos += speed;
+
+        }
+
+
+        if (e.keyCode == 65) {
+
+            requestAnimationFrame(function () {
+                clearInterval(timer);
+                animate(0,6,3);
+            });
+
+            xPos -= speed;
+
+
+        }
+
+
+    }
+
+    
 
