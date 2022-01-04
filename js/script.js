23f1d8db5555db64ad582ebd2c0d97b77d45116f
MajkeltCZe <majkeltcz@gmail.com>
2022-01-04 20:17:27 +0100

    narration

--- js/script.js
+++ js/script.js
@@ -2,49 +2,192 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const narrator = document.getElementById('narrator');
 const options = document.getElementById('options');
-const button = document.querySelectorAll('.button');
-document.addEventListener('keyup', keyboardPress);
-document.addEventListener('keydown', keyboard);
 const character = new Image();
 var background = new Image();
+const waveyMan = new Image();
 character.src = './img/test1.png';
+waveyMan.src = './img/waveman.png'
 background.src = './img/background0.jpg';
 const play = document.getElementById('play');
-const menu = document.getElementById('menu');
 const game = document.getElementById('game');
 const frameWidth = 108;
 const frameHeight = 140;
 
 var xPos = 20;
 var yPos = 280;
-var speed = 7;
+var speed = 6;
 let frameIndex = 0;
 let count = 0;
 var timer;
 let bg = 0;
+let playable;
+let story = 1;
 
-let state = {};
 
-
-
-background.onload = function () {
-
-    ctx.drawImage(background, 0, 0);
-    ctx.drawImage(character, frameWidth, 4 * frameHeight, frameWidth, frameHeight, xPos, yPos, frameWidth, frameHeight);
-
-}
 
 
 play.addEventListener('click', () => {
 
     game.style.display = "initial";
-    menu.style.display = "none";
-    document.body.style.backgroundImage = "url('sdw')";
+    play.style.display = "none";
+    document.body.style.backgroundImage = "url(./img/backgrounds.jpg)";
+
+    ctx.drawImage(background, 0, 0);
+    ctx.drawImage(character, frameWidth, 4 * frameHeight, frameWidth, frameHeight, xPos, yPos, frameWidth, frameHeight);
+    Narration(1);
+
 });
 
 
+//narration
+// ----------------------------------------------------------------------------------------// 
 
-//walking
+function Narration(textIndex) {
+    const textNode = texts.find(textNode => textNode.id === textIndex);
+    narrator.innerHTML = textNode.text;
+
+    while (options.firstChild) {
+        options.removeChild(options.firstChild)
+    }
+
+    if (Object.keys(textNode.options).length === 0) {
+
+        playable = true;
+    }
+
+    else {
+        playable = false;
+    }
+
+
+    textNode.options.forEach(option => {
+        const button = document.createElement('button')
+        button.innerText = option.text
+        button.classList.add('btn');
+        button.addEventListener('click', () => selectOption(option))
+        options.appendChild(button);
+
+    });
+}
+
+function selectOption(option) {
+    const nextTextNodeId = option.nextText;
+    Narration(nextTextNodeId);
+    playable = true;
+
+}
+
+
+const texts = [
+    {
+        id: 1,
+        text: '<p><strong class="narrator">Narrator:</strong> Welcome ! </p>  <p>Choose answer below by clicking on it:</p>'
+        ,
+        options: [
+            {
+                text: 'Hi',
+                nextText: 2
+            },
+            {
+                text: 'Ignore',
+                nextText: 3
+            }
+        ]
+    },
+
+    {
+        id: 2,
+        text: ' <p><strong class="narrator">Narrator:</strong> Press <strong>"A"</strong> to move left! Press <strong>"D"</strong> to move right!</p>',
+        options: [
+
+
+        ]
+    },
+
+    {
+        id: 3,
+        text: '<p><strong class="narrator">Narrator:</strong> Rude...Figure how to move yourself. </p>'
+        , options: [
+        ]
+
+    },
+
+    {
+        id: 4,
+        text: '<p><strong class="narrator">Narrator:</strong> Do you wanna hear your backstory ? </p>'
+        , options: [
+        
+            {
+                text: 'Yes',
+                nextText: 5
+            },
+            {
+                text: 'What, no.',
+                nextText: 6
+            }
+        
+        ]
+
+    },
+
+    {
+        id: 5,
+        text: '<p><strong class="narrator">Narrator:</strong> You are Jako and you just wanted to live your life, but you are also adventurous so here is your adventure! </p>'
+        , options: []
+
+    },
+
+
+    {
+        id: 6,
+        text: '<p><strong class="narrator">Narrator:</strong> .... </p>'
+        , options: []
+
+    },
+
+
+    {
+        id: 7,
+        text: '<p><strong class="narrator">Narrator:</strong> You meet a person. What will you do ? </p>'
+        , options: [
+            
+            {
+                text: 'Wave at him',
+                nextText: 8
+            },
+            {
+                text: 'Do nothing',
+                nextText: 9
+            }
+
+
+
+
+        ]
+
+    },
+
+
+
+
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+//animation
+// ----------------------------------------------------------------------------------------// 
 function animate(startIndex, endIndex, row) {
     count++;
     if (count > 3) {
@@ -79,31 +222,31 @@ function interval() {
     animate(0, 0, 4);
 }
 
-console.log(playable);
 
 
+//keyboard
+// ----------------------------------------------------------------------------------------// 
+document.addEventListener('keyup', keyboardPressed);
+document.addEventListener('keydown', keyboard);
 
-    //keyboard
-    function keyboardPress(b) {
 
-        if (b.keyCode == 39 || b.keyCode == 37) {
+function keyboardPressed(e) {
+
+    if (e.keyCode == 39 || e.keyCode == 37) {
 
 
-            timer = setInterval(interval, 50);
-
-        }
+        timer = setInterval(interval, 50);
 
     }
 
+}
 
+function keyboard(e) {
+    if (e.keyCode == 39) {
 
-    function keyboard(e) {
-        if (e.keyCode == 39) {
-
-
+        if (playable == true) {
             requestAnimationFrame(function () {
                 clearInterval(timer);
-
                 animate(0, 4, 2);
             });
 
@@ -116,9 +259,10 @@ console.log(playable);
                 xPos += speed;
             }
         }
+    }
 
-        if (e.keyCode == 37) {
-
+    if (e.keyCode == 37) {
+        if (playable == true) {
             requestAnimationFrame(function () {
                 clearInterval(timer);
 
@@ -133,11 +277,14 @@ console.log(playable);
                 xPos -= speed;
             }
         }
-        ChangeBackground();
-
     }
+    ChangeBackground();
+Storytelling();
 
+}
 
+//background Change
+// ----------------------------------------------------------------------------------------// 
 
 function ChangeBackground() {
 
@@ -158,92 +305,26 @@ function ChangeBackground() {
 
 }
 
-//texty 
+
+function Storytelling() {
 
 
-function Narration (number) {
-    const textNode = texts.find(textNode => textNode.id === textNodeIndex)
-narrator.innerHTML = textNode.text
+if (bg == 0 && story == 1 && xPos >= 400) {
+story++;
+    Narration(4);
+
+}
+
+if (bg ==1 && story == 2) {
+story++;
+
+Narration(7);
+}
 
 
 }
 
 
-/*
-
-function Narration(textNodeIndex) {
-    const textNode = texts.find(textNode => textNode.id === textNodeIndex)
-    narrator.innerHTML = textNode.text
-
-
-    textNode.options.forEach(option => {
-        if (showOption(option)) {
-            const button = document.createElement('button');
-            button.innerText = option.text;
-            button.addEventListener('click', () => selectOption(option))
-
-        }
-    });
-
-
-}
-
-function showOption(option) {
-    return option.requiredState == null || option.requiredState(state)
-}
-
-function selectOption(option) {
-    const nextTextNodeId = option.nextText
-
-    state = Object.assign(state, option.setState)
-    Narration(nextTextNodeId);
-
-}
 
 
 
-//texty
-const texts = [
-    {
-        id: 1,
-        text: ` <p><strong class="narrator">Narrator:</strong> Welcome ! </p> <p>Press <strong>"A"</strong> to move left , press <strong>"D"</strong> to move right.</p>
-        <p>Choose answer below by clicking on it:</p>`
-        ,
-        options: [
-            {
-                text: 'Hi',
-                nextText: 2
-            },
-            {
-                text: 'Ignore',
-                setState: { Rude: true },
-
-                nextText: 3
-            }
-        ]
-    },
-
-    {
-        id: 2,
-        text: 'You venture forth in search of answers to where you are when you come across a merchant.',
-        options: [
-            {
-                text: 'Trade the goo for a sword',
-                setState: { blueGoo: false, sword: true },
-                nextText: 3
-            },
-            {
-                text: 'Trade the goo for a shield',
-                setState: { blueGoo: false, shield: true },
-                nextText: 3
-            }
-
-        ]
-    }
-
-
-]
-
-
-Narration(1);
-*/
